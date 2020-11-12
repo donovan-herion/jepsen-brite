275a725d52a34496544ca42f33f3ed0c4137d77f
donovanherion <donovanherion@gmail.com>
2020-11-12 14:56:06 +0100

    pushed to heroku

--- pdo.php
+++ pdo.php
@@ -1,5 +1,6 @@
 <?php
-$bdd = new PDO('mysql:host=localhost;dbname=event_manager;charset=utf8', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+$bdd = new PDO('mysql:host=eu-cdbr-west-03.cleardb.net;dbname=heroku_7c83cd5d54aceb3;charset=utf8', 'bc9e14766da21b', 'a1c18af0', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 ?>
 
+<!-- mysql://bc9e14766da21b  :  a1c18af0 @   eu-cdbr-west-03.cleardb.net   /heroku_7c83cd5d54aceb3   ?reconnect=true -->
 <!-- finish -->
